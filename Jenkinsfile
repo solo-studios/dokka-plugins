d8a241b1c8eb340f6b0a1868027f23f55e88a2b6
solonovamax <solonovamax@12oclockpoint.com>
2024-08-15 16:43:48 -0400

    Okay try and set force snapshots in the Jenkinsfile (50/50 chance this breaks)
    Signed-off-by: solonovamax <solonovamax@12oclockpoint.com>

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,7 +2,7 @@
  * Copyright (c) 2024-2024 solonovamax <solonovamax@12oclockpoint.com>
  *
  * The file Jenkinsfile is part of dokka-plugins
- * Last modified on 15-08-2024 04:00 p.m.
+ * Last modified on 15-08-2024 04:36 p.m.
  *
  * MIT License
  *
@@ -34,6 +34,10 @@ pipeline {
 
     triggers {
         githubPush()
+    }
+
+    enviroment {
+        ORG_GRADLE_PROJECT_release.forceSnapshot = true
     }
 
     stages {
--- buildSrc/src/main/kotlin/dokka-plugins.versioning.gradle.kts
+++ buildSrc/src/main/kotlin/dokka-plugins.versioning.gradle.kts
@@ -2,7 +2,7 @@
  * Copyright (c) 2023-2024 solonovamax <solonovamax@12oclockpoint.com>
  *
  * The file dokka-plugins.versioning.gradle.kts is part of dokka-plugins
- * Last modified on 15-08-2024 04:23 p.m.
+ * Last modified on 15-08-2024 04:27 p.m.
  *
  * MIT License
  *
@@ -34,7 +34,6 @@ scmVersion {
         prefix = project.name
         versionSeparator = "-v"
     }
-    forceSnapshot()
 }
 
 project.version = scmVersion.version
